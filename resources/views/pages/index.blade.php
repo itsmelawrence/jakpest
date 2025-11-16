5bf96105545ee71fc4e74e1cea230150078b05e9
Lawrence Tendenilla <lawrencetendenilla83@gmail.com>
2025-11-16 13:35:58 +0800

    fix: container spacing and slider font size

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
         <div class="hero-content">
             <h1>Keep Your Home Safe With <span class="highlight-text">JAK Pest Control Services</span></h1>
             <p>At JAK Pest Control Services, we provide safe, effective, and long-lasting solutions against pests in
-                Valenzuela City and across the Luzon region—protecting your home, your family, and your peace of mind.  
+                Valenzuela City and across the Luzon region—protecting your home, your family, and your peace of mind.
             </p>
             <div class="menu-item">
                 <a href="#" class="button-link">Schedule a Service Today</a>
@@ -19,7 +19,11 @@
         </div>
         <div class="split-content">
             <h2>Protect Your Home From Pesky Intruders</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
             <div class="menu-item"><a href="#" class="button-link">Learn How We Work</a></div>
         </div>
     </div>
@@ -32,7 +36,11 @@
         </div>
         <div class="split-content">
             <h2>Why JAK Pest Control Services?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
             <div class="menu-item"><a href="#" class="button-link">Learn How We Work</a></div>
         </div>
     </div>
@@ -42,7 +50,9 @@
     <div class="layout-section custom-slider-section layout-split-content">
         <div class="split-content">
             <h2>Comprehensive Pest Solutions for Every Home and Business</h2>
-            <p>Whether you’re protecting your family’s living space or securing a commercial property, our specialized services keep your environment safe, healthy, and pest-free. Explore our key offerings designed to eliminate threats and prevent future infestations.</p>
+            <p>Whether you’re protecting your family’s living space or securing a commercial property, our specialized
+                services keep your environment safe, healthy, and pest-free. Explore our key offerings designed to
+                eliminate threats and prevent future infestations.</p>
         </div>
         <div class="swiper">
             <div class="swiper-wrapper">
@@ -54,9 +64,12 @@
                         </div>
                         <div class="custom-slider-content">
                             <h3>Bed Bugs Control</h3>
-                            <p>Eliminate bed bugs quickly and safely from your home or business. Our expert treatments target every hiding spot, ensuring a pest-free sleep environment.</p>
-                            <div class="menu-item">
-                                <a href="#" class="button-link-swiper">Learn more</a>
+                            <div class="row-group">
+                                <p>Eliminate bed bugs quickly and safely from your home or business. Our expert
+                                    treatments target every hiding spot, ensuring a pest-free sleep environment.</p>
+                                <div class="menu-item">
+                                    <a href="#" class="button-link-swiper">Learn more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -69,9 +82,14 @@
                         </div>
                         <div class="custom-slider-content">
                             <h3>Disinfection</h3>
-                            <p>Keep your space hygienic and safe. We provide professional disinfection services that remove harmful bacteria, viruses, and allergens for a cleaner, healthier environment.</p>
-                            <div class="menu-item">
-                                <a href="#" class="button-link-swiper">Learn more</a>
+                            <div class="row-group">
+                                <p>Keep your space hygienic and safe. We provide professional disinfection services that
+                                    remove harmful bacteria, viruses, and allergens for a cleaner, healthier
+                                    environment.
+                                </p>
+                                <div class="menu-item">
+                                    <a href="#" class="button-link-swiper">Learn more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -84,9 +102,13 @@
                         </div>
                         <div class="custom-slider-content">
                             <h3>General Pest Control</h3>
-                            <p>Protect your home or business from common pests such as ants, cockroaches, and rodents. Our tailored solutions prevent infestations and maintain a safe living space.</p>
-                            <div class="menu-item">
-                                <a href="#" class="button-link-swiper">Learn more</a>
+                            <div class="row-group">
+                                <p>Protect your home or business from common pests such as ants, cockroaches, and
+                                    rodents.
+                                    Our tailored solutions prevent infestations and maintain a safe living space.</p>
+                                <div class="menu-item">
+                                    <a href="#" class="button-link-swiper">Learn more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -99,9 +121,12 @@
                         </div>
                         <div class="custom-slider-content">
                             <h3>Termite Control</h3>
-                            <p>Prevent costly structural damage with our specialized termite treatments. We detect, eliminate, and safeguard your property from termite invasions.</p>
-                            <div class="menu-item">
-                                <a href="#" class="button-link-swiper">Learn more</a>
+                            <div class="row-group">
+                                <p>Prevent costly structural damage with our specialized termite treatments. We detect,
+                                    eliminate, and safeguard your property from termite invasions.</p>
+                                <div class="menu-item">
+                                    <a href="#" class="button-link-swiper">Learn more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -114,14 +139,17 @@
                         </div>
                         <div class="custom-slider-content">
                             <h3>Tick and Flea Control</h3>
-                            <p>Keep your family and pets safe with our targeted tick and flea treatments. Fast, effective, and eco-friendly solutions for a pest-free home.</p>
-                            <div class="menu-item">
-                                <a href="#" class="button-link-swiper">Learn more</a>
+                            <div class="row-group">
+                                <p>Keep your family and pets safe with our targeted tick and flea treatments. Fast,
+                                    effective, and eco-friendly solutions for a pest-free home.</p>
+                                <div class="menu-item">
+                                    <a href="#" class="button-link-swiper">Learn more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
                 <!-- If we need pagination -->
                 <div class="swiper-pagination"></div>
 
@@ -133,4 +161,10 @@
                 <div class="swiper-scrollbar"></div>
             </div>
         </div>
+</section>
+
+<section class="main-container">
+    <div class="layout-section">
+
+    </div>
 </section>
